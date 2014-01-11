9ba5ac69832f5fd8eb1713b9e73830bdbf84116a
Melissa Carbone <mcarbone1@gmail.com>
2014-01-11 12:51:31 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quoteable::Application.config.secret_key_base = '21c6a4ed520acaaf67bf39ef82ffa8d737acfafd853bf48c33b1c793d18034a099f7d082c486d8e6a7c5578df96b707e133848c606b178c2b20c8f1fa8a06693'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Quoteable::Application.config.session_store :cookie_store, key: '_quoteable_session'
