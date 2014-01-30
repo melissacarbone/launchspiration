0d6e5dd0da7f2777fb954d99225d7f3a2d3401b5
Melissa Carbone <mcarbone1@gmail.com>
2014-01-30 16:33:20 -0500

    User views a random quote

--- spec/features/view_quote_spec.rb
+++ spec/features/view_quote_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
-features 'user views a quote' do
-
+feature 'User views a quote' do
+  before do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.add_mock :twitter, uid: "12345", info: { name: "Jane Smith"}
+    Quote.create(content: 'This is a quote', author: 'Jane Smith', cohort: 'Winter 2013')
+  end
+  scenario 'after authenticating' do
+    visit root_path
+    click_link "Sign in with Twitter"
+    click_link "Get Started!"
+    save_and_open_page
+    expect(page).to have_content("Need some inspiration? Click Here!")
+  end
 end
